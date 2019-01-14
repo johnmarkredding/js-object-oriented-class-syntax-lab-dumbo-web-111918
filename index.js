7d28494f6d5ea25b6c7439c6f43034f3f348aca3
John Mark Redding <johnmarkredding@gmail.com>
2019-01-14 15:04:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class BoardMember {
     return "I like to help people.";
   }
   releasePressStatement() {
-    return "I like to help people.";
+    return "You will see great things from Scuber.";
   }
   
 }
