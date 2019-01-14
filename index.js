2aa43b6e641042fe16f64f34be1a02170d6bb37f
John Mark Redding <johnmarkredding@gmail.com>
2019-01-14 15:05:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class BoardMember {
     return "You will see great things from Scuber.";
   }
   sayHi() {
-    return "You will see great things from Scuber.";
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
   }
   
 }
